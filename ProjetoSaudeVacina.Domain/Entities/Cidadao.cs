9ed9c683047baedacf77543781fb16bd8a8bed50
Felipe Melo <felipemdmelo@gmail.com>
2018-08-21 16:26:08 -0300

    Adicionando comentários na classe da entidade Cidadao.

--- ProjetoSaudeVacina.Domain/Entities/Cidadao.cs
+++ ProjetoSaudeVacina.Domain/Entities/Cidadao.cs
@@ -1,5 +1,10 @@
 ﻿namespace ProjetoSaudeVacina.Domain.Entities
 {
+    /*
+     * Entidade que armazena as informações do cidadão
+     * Cidadão consulta os postos de saúde próximos de sua localização atual
+     * Cidadão consulta o estoque dos postos de saúde
+     */
     public class Cidadao : Pessoa
     {
         
